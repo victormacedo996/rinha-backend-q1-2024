640e00bd10d7161f4bfcf23fcee28c39f09d3094
victor macedo <victormacedo996@gmail.com>
2024-02-07 21:26:14 -0300

    feat: add routers

--- api/internal/webserver/http/chi/router/router.go
+++ api/internal/webserver/http/chi/router/router.go
@@ -1,1 +1,21 @@
 package router
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/victormacedo996/rinha-backend-q1-2024/internal/infrastructure/database/postgres"
+	"github.com/victormacedo996/rinha-backend-q1-2024/internal/infrastructure/database/redis"
+	"github.com/victormacedo996/rinha-backend-q1-2024/internal/webserver/http/chi/validator"
+)
+
+func setUpRoutes(r chi.Mux) {
+
+	validator := validator.GetInstance()
+	db := postgres.GetInstane()
+	redis := redis.GetInstance()
+
+	r.Post("/clientes/{id}/transacoes", createTransaction(validator, db, redis))
+	r.Get("/clientes/{id}/extrato", getBankStatement)
+
+	r.Get("/health", health)
+
+}

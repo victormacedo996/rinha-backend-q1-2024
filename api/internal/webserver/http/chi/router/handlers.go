f050e8f06e5bf11ee0cee401e35180cf4937ce40
victor macedo <victormacedo996@gmail.com>
2024-02-07 21:26:30 -0300

    feat: add handlers

--- api/internal/webserver/http/chi/router/handlers.go
+++ api/internal/webserver/http/chi/router/handlers.go
@@ -4,37 +4,77 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
 	"github.com/victormacedo996/rinha-backend-q1-2024/internal/infrastructure/database/postgres"
+	"github.com/victormacedo996/rinha-backend-q1-2024/internal/infrastructure/database/redis"
 	dto "github.com/victormacedo996/rinha-backend-q1-2024/internal/webserver/http/chi/dto/request"
 	"github.com/victormacedo996/rinha-backend-q1-2024/internal/webserver/http/chi/response"
-	"github.com/victormacedo996/rinha-backend-q1-2024/internal/webserver/http/chi/validator"
 )
 
-func createTransaction(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "id")
-	if id == "" {
-		response.StatusBadRequest(w, r, errors.New("empty id"))
+func createTransaction(validator *validator.Validate, db *postgres.DbInstance, redis *redis.Redis) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		urlId := chi.URLParam(r, "id")
+		if urlId == "" {
+			response.StatusBadRequest(w, r, errors.New("Empty id"))
+			return
+		}
+
+		id, err := strconv.Atoi(urlId)
+		if err != nil {
+			response.StatusBadRequest(w, r, errors.New("Invalid id"))
+			return
+		}
+
+		if id > 5 {
+			response.StatusNotFound(w, r, errors.New("User not found"))
+			return
+		}
+
+		var incoming_transaction dto.TransactionRequest
+
+		err = json.NewDecoder(r.Body).Decode(&incoming_transaction)
+		if err != nil {
+			response.StatusBadRequest(w, r, errors.New("Error parsing request body"))
+			return
+		}
+
+		err = validator.Struct(incoming_transaction)
+		if err != nil {
+			response.StatusBadRequest(w, r, errors.New("Error validating request body"))
+			return
+		}
+
+		for {
+			lock := redis.GetDbLock()
+			if lock == "" {
+				break
+			}
+		}
+
+		redis.LockDb()
+
+		transaction_response, err := db.RegisterTransaction(r.Context(), id, incoming_transaction)
+		if err != nil {
+			response.StatusInternalServerError(w, r, errors.New("Error creating transaction"))
+			return
+		}
+		redis.UnlockDb()
+
+		response.StatusOk(w, r, transaction_response)
 		return
 	}
+}
 
-	var incoming_transaction dto.TransactionRequest
+func getBankStatement(w http.ResponseWriter, r *http.Request) {
+	response.StatusOk(w, r, map[string]interface{}{"status": "ok"})
+	return
+}
 
-	err := json.NewDecoder(r.Body).Decode(&incoming_transaction)
-	if err != nil {
-		response.StatusBadRequest(w, r, errors.New("Error parsing request body"))
-		return
-	}
-
-	validator := validator.GetInstance()
-
-	err = validator.Struct(incoming_transaction)
-	if err != nil {
-		response.StatusBadRequest(w, r, errors.New("Error validating body"))
-		return
-	}
-
-	db := postgres.GetInstane()
-
+func health(w http.ResponseWriter, r *http.Request) {
+	response.StatusOk(w, r, map[string]interface{}{"status": "ok"})
+	return
 }

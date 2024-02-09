a597c118a081c4b98c5ff731d67dcd131e4e105f
victor macedo <victormacedo996@gmail.com>
2024-02-08 22:11:36 -0300

    refactor: make validator public

--- api/internal/webserver/http/chi/validator/validator.go
+++ api/internal/webserver/http/chi/validator/validator.go
@@ -8,7 +8,7 @@ import (
 
 var once sync.Once
 
-var validator *playValidator.Validate
+var Validator *playValidator.Validate
 
 func validateCreditOrDebit(fl playValidator.FieldLevel) bool {
 	r, ok := fl.Field().Interface().(rune)
@@ -20,16 +20,16 @@ func validateCreditOrDebit(fl playValidator.FieldLevel) bool {
 }
 
 func GetInstance() *playValidator.Validate {
-	if validator == nil {
+	if Validator == nil {
 		once.Do(
 			func() {
-				validator = playValidator.New()
-				validator.RegisterValidation("validateCreditOrDebit", validateCreditOrDebit)
+				Validator = playValidator.New()
+				Validator.RegisterValidation("validateCreditOrDebit", validateCreditOrDebit)
 
 			},
 		)
 	}
 
-	return validator
+	return Validator
 
 }

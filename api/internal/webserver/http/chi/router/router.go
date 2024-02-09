8497af4d673a3da5dcb3b971ccaa96488a6d4dc8
victor macedo <victormacedo996@gmail.com>
2024-02-08 22:11:03 -0300

    feat: add start webserver function

--- api/internal/webserver/http/chi/router/router.go
+++ api/internal/webserver/http/chi/router/router.go
@@ -1,21 +1,31 @@
 package router
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-playground/validator/v10"
+	"github.com/victormacedo996/rinha-backend-q1-2024/internal/config"
 	"github.com/victormacedo996/rinha-backend-q1-2024/internal/infrastructure/database/postgres"
 	"github.com/victormacedo996/rinha-backend-q1-2024/internal/infrastructure/database/redis"
-	"github.com/victormacedo996/rinha-backend-q1-2024/internal/webserver/http/chi/validator"
 )
 
-func setUpRoutes(r chi.Mux) {
-
-	validator := validator.GetInstance()
-	db := postgres.GetInstane()
-	redis := redis.GetInstance()
-
+func setUpRoutes(r *chi.Mux, db *postgres.DbInstance, redis *redis.Redis, validator *validator.Validate) {
+	r.Use(middleware.Logger)
 	r.Post("/clientes/{id}/transacoes", createTransaction(validator, db, redis))
-	r.Get("/clientes/{id}/extrato", getBankStatement)
+	r.Get("/clientes/{id}/extrato", getBankStatement(db, redis))
 
 	r.Get("/health", health)
 
+}
+
+func Start(db *postgres.DbInstance, redis *redis.Redis, validator *validator.Validate) {
+	router := chi.NewRouter()
+	setUpRoutes(router, db, redis, validator)
+
+	c := config.GetInstance()
+	port := fmt.Sprintf(":%v", c.WebServer.SERVER_PORT)
+	http.ListenAndServe(port, router)
 }

5a8dfe4aded8b2cb35a968e3cb95bd548a86b906
victor macedo <victormacedo996@gmail.com>
2024-02-11 22:32:20 -0300

    refactor: move dto folder

--- api/internal/dto/request/transaction.go
+++ api/internal/dto/request/transaction.go
@@ -3,5 +3,5 @@ package dto
 type TransactionRequest struct {
 	Value       int    `json:"valor"`
 	Type        string `json:"tipo" validate:"eq=c|eq=d"`
-	Description string `json:"descricao" validate:"max=10"`
+	Description string `json:"descricao" validate:"min=1,max=10"`
 }

4b54e7c629c86b344a2e2b96e3ec688379222e89
victor macedo <victormacedo996@gmail.com>
2024-02-13 18:28:06 -0300

    refactor: remove logs from main

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/victormacedo996/rinha-backend-q1-2024/internal/config"
 	"github.com/victormacedo996/rinha-backend-q1-2024/internal/infrastructure/database/postgres"
 	"github.com/victormacedo996/rinha-backend-q1-2024/internal/infrastructure/database/redis"
 	"github.com/victormacedo996/rinha-backend-q1-2024/internal/webserver/http/chi/router"
@@ -16,16 +14,12 @@ func main() {
 	db := postgres.GetInstane()
 	redis := redis.GetInstance()
 	validator := validator.GetInstance()
-	c := config.GetInstance()
 	ctx := context.Background()
-	fmt.Println("unlocking db")
 	err := redis.UnlockDb(ctx)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("db unlocked")
 
 	router.Start(db, redis, validator)
-	fmt.Println(fmt.Sprintf("started api0%s", c.WebServer.API_ID))
 
 }

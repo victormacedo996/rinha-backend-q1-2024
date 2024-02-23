ffb7f9174c0b8f80d1e1b049fd0015164d8bd7c3
victor macedo <victormacedo996@gmail.com>
2024-02-22 21:09:46 -0300

    refactor: change input and ouput types

--- api/internal/infrastructure/database/interface.go
+++ api/internal/infrastructure/database/interface.go
@@ -3,13 +3,12 @@ package database
 import (
 	"context"
 
-	dtoRequest "github.com/victormacedo996/rinha-backend-q1-2024/internal/dto/request"
-	dtoResponse "github.com/victormacedo996/rinha-backend-q1-2024/internal/dto/response"
+	"github.com/victormacedo996/rinha-backend-q1-2024/internal/infrastructure/database/entity"
 )
 
 type Database interface {
 	GetClientBalanceAndLimit(context.Context, int) (int, int, error)
 	UpdateClientBalance(context.Context, int, int) error
-	RegisterTransaction(context.Context, int, dtoRequest.TransactionRequest) error
-	GetBankStatement(context.Context, int) (*dtoResponse.BankStatement, error)
+	RegisterTransaction(context.Context, int, entity.TransactionRequest) error
+	GetBankStatement(context.Context, int) (*entity.BankStatement, error)
 }
